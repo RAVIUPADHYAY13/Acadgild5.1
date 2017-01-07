f5cd155092d905b1f5ad3ab071fef84bec5812a1
Shubham Agarwal <shubhgrwlgeniee@gmail.com>
2017-01-07 17:54:23 +0530

    Add files via upload

---
+++ javascript/app.js
@@ -0,0 +1,5 @@
+var name = "Shubham Agarwal"; 
+var age  = 21; 
+var dob = "11 Sep 1995";
+var pob = "New Delhi";
+console.log("Name: "+ name + "\nAge: "+ age +"\nDate of birth: " + dob + "\nPlace of birth: " + pob ); 
